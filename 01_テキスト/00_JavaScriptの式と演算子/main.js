a215ec9bc9ea473209051104d865f5b871fbfce3
otochan <oto10ripr8gs@gmail.com>
2023-10-14 15:16:05 +0900

    [@3nobaisu]wogakusyushimashita.

--- 01_テキスト/00_JavaScriptの式と演算子/main.js
+++ 01_テキスト/00_JavaScriptの式と演算子/main.js
@@ -1,8 +1,11 @@
 // TRY: 練習問題１
-
+console.log(57052 + 57054, "114" + "106")
 // TRY: 練習問題２
+let x = 5
+console.log((x += 2), (x *= 5), (x = 100 % x), x) //?
 
 // TRY: 練習問題３
+
 let happiness = 0 // 0 から 100 までの数値を代入してください
 let sleepiness = 0 // 0 から 100 までの数値を代入してください
 
@@ -12,6 +15,9 @@ const isSleepy = sleepiness > 90
 const isNotHappyAndSleepy = !isHappy && isSleepy
 
 // 以下のコメントアウトを外して確認してみましょう
-// console.log("isNotHappyAndSleepy" + "は" + true + "ですか？", isNotHappyAndSleepy)
-// console.log("isHappy", isHappy)
-// console.log("isSleepy", isSleepy)
+console.log(
+  "isNotHappyAndSleepy" + "は" + true + "ですか？",
+  isNotHappyAndSleepy,
+)
+console.log("isHappy", isHappy)
+console.log("isSleepy", isSleepy)
