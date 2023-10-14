dac85d0b11951b36756852e5e7d8c7fa9761edbd
otochan <oto10ripr8gs@gmail.com>
2023-10-15 01:19:00 +0900

    [カウンター]を学習しました

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,27 @@
+const button = document.getElementById("button")
+const alertMessage = function () {
+  alert("クリックしたね")
+}
+
+button.onclick = alertMessage()
+
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("input-date")
+
+const sayHello = function () {
+  console.log("hello")
+}
+
+inputText.oninput = sayHello
+inputText.oninput = sayHello
+
+const logValue = function (e) {
+  console.log(e.target.value)
+}
+
+inputText.oninput = logValue
+inputDate.oninput = logValue
+
+document.onkeydown = function (e) {
+  console.log(e.key)
+}
